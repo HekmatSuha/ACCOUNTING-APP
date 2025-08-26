8c3c4d8bb2e2de881ae3c912c56a9b2ecc6f5313
HekmatSuha <76567609+HekmatSuha@users.noreply.github.com>
2025-08-26 13:47:32 +0600

    Allow uploading product images

--- backend/api/models.py
+++ backend/api/models.py
@@ -191,6 +191,7 @@ class Product(models.Model):
     purchase_price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     sale_price = models.DecimalField(max_digits=12, decimal_places=2)
     stock_quantity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
